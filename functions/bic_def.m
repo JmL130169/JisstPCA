82fbcf89ca44564f52925c5820fb00b0b48f767d
Jiaming <101954229+JmL130169@users.noreply.github.com>
2023-04-06 21:27:00 -0500

    Add files via upload

---
+++ functions/bic_def.m
@@ -0,0 +1,77 @@
+% BIC method for multi-factor rank selection based on single-factor BIC
+% with subtract deflation
+
+% input of bic_def:
+% X, Y: multi-factor semi-symmetric tensors
+% rank_max: the largest value of rank you want to try. For example, when p,
+% q are large, it takes long time to try all different possible ranks. As
+% we only care about the low rank structure of tensor factors Vk and Wk, we
+% can set rank_max = 5.
+% K: number of layers
+% u0, lambda, tol, max_iter: the input needed in Jisst_single
+% method: if method = 1, we do not assume X and Y are of the same rank.
+% When method = 2, we assume they have the same rank
+
+% output of bic_def:
+% bic_rx, bic_ry: best rank selected by minimizing BIC. bic_rx, bic_ry are vectors of ranks
+% for each Vk, Wk, so bic_rx, bic_ry are of dimension K
+
+% Note: this function takes long time. When applying this function, you
+% might assume the rank of X equals the rank of Y by inputting method = 2
+
+function [bic_rx, bic_ry] = bic_def(X, Y, rank_max, K, u0, lambda, tol, max_iter, method)
+
+    bic_rx = ones(K, 1); % K is the number of layers, which is prespecified
+    bic_ry = ones(K, 1);
+    X_iter = cell(K+1, 1);
+    Y_iter = cell(K+1, 1);
+    
+    k = 1;
+    X_iter{1} = X;
+    Y_iter{1} = Y;
+
+    if method == 1
+        while k < K+1
+            bic = ones(rank_max, rank_max);
+
+            for i = 1:rank_max
+                for j = 1:rank_max
+                    bic(i, j) = bic_sst(i, j, X_iter{k}, Y_iter{k}, u0, lambda, tol, max_iter);
+                end
+            end
+            [~, bic_min] = min(bic);
+            bic_rx(k) = bic_min(1); % BIC estimation for the kth layer, X
+            bic_ry(k) = bic_min(2); % BIC estimation for the kth layer, Y
+
+            [hat_u, ~, ~, ~, ~, hat_X, hat_Y] = Jisst_single(X_iter{k}, Y_iter{k}, u0, bic_rx(k), bic_ry(k), lambda, tol, max_iter);
+            X_iter{k+1} = X_iter{k} - hat_X;
+            Y_iter{k+1} = Y_iter{k} - hat_Y; % subtract deflation of the next layer
+            u0 = hat_u;
+
+            k = k + 1;
+        end
+
+    elseif method == 2
+        while k < K+1
+            bic = ones(rank_max, 1);
+
+            for i = 1:rank_max
+                bic(i) = bic_sst(i, i, X_iter{k}, Y_iter{k}, u0, lambda, tol, max_iter);
+            end
+            [~, bic_min] = min(bic);
+            bic_rx(k) = bic_min; % BIC estimation for the kth layer, X
+            bic_ry(k) = bic_min; % BIC estimation for the kth layer, Y
+
+            [hat_u, ~, ~, ~, ~, hat_X, hat_Y] = Jisst_single(X_iter{k}, Y_iter{k}, u0, bic_rx(k), bic_ry(k), lambda, tol, max_iter);
+            X_iter{k+1} = X_iter{k} - hat_X;
+            Y_iter{k+1} = Y_iter{k} - hat_Y; % subtract deflation of the next layer
+            u0 = hat_u;
+
+            k = k+1;
+        end
+    end
+end
+
+% Note: the output of bic_def might not be directly used in Jisst_multi or
+% dJisst_multi because bic_rx, bic_ry we obtained are column vectors. We
+% might need to simply transpose the output before using them.
---
+++ functions/bic_sst.m
@@ -0,0 +1,50 @@
+% This function calculate value of BIC when the input rank is rx and ry in
+% single factor case. We always select rx, ry that have smallest BIC as the
+% input ranks.
+
+% input of bic_sst:
+% rx, ry: possible rank of single factor X and Y
+% X, Y: single factor semi-symmetric tensors
+% u0: initialization for implementing Jisst_single
+% lambda: scaler, prespecified or data-driven
+% tol, max_iter: inputs that needed in Jisst_single
+
+% output of bic_sst:
+% bic_val: the corresponding BIC value for rank rx and ry, given data X and Y
+
+function [bic_val] = bic_sst(rx, ry, X, Y, u0, lambda, tol, max_iter)
+    
+    sz_x = size(X);
+    sz_y = size(Y);
+
+    n1 = sz_x(1)*sz_x(2)*sz_x(3);
+    n2 = sz_y(1)*sz_y(2)*sz_y(3);
+    n = n1 + n2; % number of observation data
+
+    pe = sum(sz_x(1)*rx + sz_y(1)*ry + sz_x(3)); % number of efficient parameters
+    
+    [~, ~, ~, ~, ~, hat_X, hat_Y] = Jisst_single(X, Y, u0, rx, ry, lambda, tol, max_iter);
+
+    Tx = tensor(zeros(sz_x));
+    Ty = tensor(zeros(sz_y));
+    
+    % log likelihood function of X based on estimation using rank rx
+    for i = 1:sz_x(1)
+        for j = 1:sz_x(2)
+            for k = 1:sz_x(3)
+                Tx(i, j, k) = (-0.5)*(log(2*pi) + ((X(i, j, k)-hat_X(i, j, k))^2));
+            end
+        end
+    end
+
+    % log likelihood function of y based on estimation using rank ry
+    for i = 1:sz_y(1)
+        for j = 1:sz_y(2)
+            for k = 1:sz_y(3)
+                Ty(i, j, k) = (-0.5)*(log(2*pi) + ((Y(i, j, k)-hat_Y(i, j, k))^2));
+            end
+        end
+    end
+
+    bic_val = pe*log(n) - 2*(sum(double(Tx), 'all') + sum(double(Ty), 'all'));
+end
