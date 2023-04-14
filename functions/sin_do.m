9a86e9486d794b3552a3d367461d64447dd56133
Jiaming <101954229+JmL130169@users.noreply.github.com>
2023-04-13 23:45:26 -0500

    Update sin_do.m

--- functions/sin_do.m
+++ functions/sin_do.m
@@ -1,5 +1,12 @@
 % sin theta distance using operator norm
 
+% input of sin_do:
+% X: low-rank matrix of dimension p-rx
+% Y: low-rank matrix of dimension p-ry
+
+% output of sin_do:
+% d: sin_theta distance between the subspace spanned by X and Y, which is operator norm of sin_theta diagonal matrix
+
 function d = sin_do(X, Y)
     [~, D, ~] = svd(X'*Y);
     D = diag(D);
