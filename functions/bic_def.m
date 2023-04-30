a2e34643df757ff20e2a6343155a923fa9acd0ec
Jiaming <101954229+JmL130169@users.noreply.github.com>
2023-04-30 01:18:35 -0500

    Update bic_def.m

--- functions/bic_def.m
+++ functions/bic_def.m
@@ -11,6 +11,7 @@
 % u0, lambda, tol, max_iter: the input needed in Jisst_single
 % method: if method = 1, we do not assume X and Y are of the same rank.
 % When method = 2, we assume they have the same rank
+% def: deflation strategy. def = 0 is subtract deflation; def = 1 is project deflation; def = 2 is subtract deflation for X and project deflation for Y; def = 3 is project deflation for X and subtract deflation for Y
 
 % output of bic_def:
 % bic_rx, bic_ry: best rank selected by minimizing BIC. bic_rx, bic_ry are vectors of ranks
@@ -19,7 +20,7 @@
 % Note: this function takes long time. When applying this function, you
 % might assume the rank of X equals the rank of Y by inputting method = 2
 
-function [bic_rx, bic_ry] = bic_def(X, Y, rank_max, K, u0, lambda, tol, max_iter, method)
+function [bic_rx, bic_ry] = bic_def(X, Y, rank_max, K, u0, lambda, tol, max_iter, method, def)
 
     bic_rx = ones(K, 1); % K is the number of layers, which is prespecified
     bic_ry = ones(K, 1);
@@ -43,9 +44,33 @@ function [bic_rx, bic_ry] = bic_def(X, Y, rank_max, K, u0, lambda, tol, max_iter
             bic_rx(k) = bic_min(1); % BIC estimation for the kth layer, X
             bic_ry(k) = bic_min(2); % BIC estimation for the kth layer, Y
 
-            [hat_u, ~, ~, ~, ~, hat_X, hat_Y] = Jisst_single(X_iter{k}, Y_iter{k}, u0, bic_rx(k), bic_ry(k), lambda, tol, max_iter);
-            X_iter{k+1} = X_iter{k} - hat_X;
-            Y_iter{k+1} = Y_iter{k} - hat_Y; % subtract deflation of the next layer
+            [hat_u, hat_V, hat_W, ~, ~, hat_X, hat_Y] = Jisst_single(X_iter{k}, Y_iter{k}, u0, bic_rx(k), bic_ry(k), lambda, tol, max_iter);
+            
+            if def == 0 % subtract deflation
+                X_iter{k+1} = X_iter{k} - hat_X;
+                Y_iter{k+1} = Y_iter{k} - hat_Y;
+            else if def == 1 % project deflation
+                sz_X = size(X);
+                sz_Y = size(Y);
+                uk = double(eye(sz_X(3)) - hat_u*hat_u');
+                Vk = double(eye(sz_X(1)) - hat_V*hat_V');
+                Wk = double(eye(sz_Y(1)) - hat_W*hat_W');
+                X_iter{k+1} = ttm(X_iter{k}, {Vk, Vk, uk}, [1, 2, 3]);
+                Y_iter{k+1} = ttm(Y_iter{k}, {Wk, Wk, uk}, [1, 2, 3]);
+            else if def == 2 % subtract deflation for X and project deflation for Y
+                X_iter{k+1} = X_iter{k} - hat_X;
+                sz_Y = size(Y);
+                uk = double(eye(sz_X(3)) - hat_u*hat_u');
+                Wk = double(eye(sz_Y(1)) - hat_W*hat_W');
+                Y_iter{k+1} = ttm(Y_iter{k}, {Wk, Wk, uk}, [1, 2, 3]);
+            else if def == 3 % project deflation for X and subtract deflation for Y
+                sz_X = size(X);
+                uk = double(eye(sz_X(3)) - hat_u*hat_u');
+                Vk = double(eye(sz_X(1)) - hat_V*hat_V');
+                X_iter{k+1} = ttm(X_iter{k}, {Vk, Vk, uk}, [1, 2, 3]);
+                Y_iter{k+1} = Y_iter{k} - hat_Y;
+            end
+            
             u0 = hat_u;
 
             k = k + 1;
@@ -62,9 +87,33 @@ function [bic_rx, bic_ry] = bic_def(X, Y, rank_max, K, u0, lambda, tol, max_iter
             bic_rx(k) = bic_min; % BIC estimation for the kth layer, X
             bic_ry(k) = bic_min; % BIC estimation for the kth layer, Y
 
-            [hat_u, ~, ~, ~, ~, hat_X, hat_Y] = Jisst_single(X_iter{k}, Y_iter{k}, u0, bic_rx(k), bic_ry(k), lambda, tol, max_iter);
-            X_iter{k+1} = X_iter{k} - hat_X;
-            Y_iter{k+1} = Y_iter{k} - hat_Y; % subtract deflation of the next layer
+            [hat_u, hat_V, hat_W, ~, ~, hat_X, hat_Y] = Jisst_single(X_iter{k}, Y_iter{k}, u0, bic_rx(k), bic_ry(k), lambda, tol, max_iter);
+            
+            if def == 0 % subtract deflation
+                X_iter{k+1} = X_iter{k} - hat_X;
+                Y_iter{k+1} = Y_iter{k} - hat_Y;
+            else if def == 1 % project deflation
+                sz_X = size(X);
+                sz_Y = size(Y);
+                uk = double(eye(sz_X(3)) - hat_u*hat_u');
+                Vk = double(eye(sz_X(1)) - hat_V*hat_V');
+                Wk = double(eye(sz_Y(1)) - hat_W*hat_W');
+                X_iter{k+1} = ttm(X_iter{k}, {Vk, Vk, uk}, [1, 2, 3]);
+                Y_iter{k+1} = ttm(Y_iter{k}, {Wk, Wk, uk}, [1, 2, 3]);
+            else if def == 2 % subtract deflation for X and project deflation for Y
+                X_iter{k+1} = X_iter{k} - hat_X;
+                sz_Y = size(Y);
+                uk = double(eye(sz_X(3)) - hat_u*hat_u');
+                Wk = double(eye(sz_Y(1)) - hat_W*hat_W');
+                Y_iter{k+1} = ttm(Y_iter{k}, {Wk, Wk, uk}, [1, 2, 3]);
+            else if def == 3 % project deflation for X and subtract deflation for Y
+                sz_X = size(X);
+                uk = double(eye(sz_X(3)) - hat_u*hat_u');
+                Vk = double(eye(sz_X(1)) - hat_V*hat_V');
+                X_iter{k+1} = ttm(X_iter{k}, {Vk, Vk, uk}, [1, 2, 3]);
+                Y_iter{k+1} = Y_iter{k} - hat_Y;
+            end
+            
             u0 = hat_u;
 
             k = k+1;
