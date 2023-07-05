a9288037b829c319e3ce663e2209d2b6336f5f7d
agenevera <agenevera@gmail.com>
2023-07-05 13:16:43 -0500

    Update init.m

--- functions/init.m
+++ functions/init.m
@@ -4,10 +4,10 @@
 % vectors/values in an decreasing order, we arrange that by hand to make
 % sure we select the singular vectors with regard to larges singular values
 
-function [u0] = init(X, Y)
+function [u0] = init(X, Y,lambda)
     
     % matricization X, Y by the third-mode and concatenate them
-    M = [double(tenmat(X, 3, [1, 2])), double(tenmat(Y, 3, [1, 2]))];
+    M = [lambda*double(tenmat(X, 3, [1, 2])), (1-lambda)*double(tenmat(Y, 3, [1, 2]))];
 
     % using the leading singular vector as spectral initialization
     [a, ~, ~] = svds(M, 1);
